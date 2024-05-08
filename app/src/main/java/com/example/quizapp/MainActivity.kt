9e19524184799d2cf26a9287ac3ce4eacb8964cd
rosa <rosa.lyubovitskaya@gmail.com>
2024-05-08 14:21:40 +0200

    started with startButton action listener

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textInputField: EditText = findViewById(R.id.textInputField)
+        var userName: String
+        val startButton: Button = findViewById(R.id.startButton)
+
+        startButton.setOnClickListener {
+            // if the user is empty, ask user to enter his name with a Toast
+            if(textInputField.text.isEmpty()) {
+                Toast.makeText(this, "Please enter your name.", Toast.LENGTH_SHORT).show()
+            }
+            // name too long
+            else if(textInputField.text.length > 30) {
+                Toast.makeText(this, "Name can't contain more than 30 characters.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                userName = textInputField.text.toString()
+                // go to next activity
+            }
+        }
     }
 }
